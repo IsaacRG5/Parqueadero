7122b4e5f2fee04cefe00698d08e0f951a03dc2b
IsaacRG5 <isaacdavid1127@gmail.com>
2026-03-17 08:13:24 -0500

    parqueadero

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-TOTAl_ESPACIOS = 10
+TOTAL_ESPACIOS = 10
 parqueadero = ["ABC123", "DEF456", "GHI789", "JKL321", "MNO654",
             "Libre", "Libre", "Libre", "Libre", "Libre"]
+
 vacio = True
+
 try:
     while vacio:
         print("\n===== PARQUEADERO =====")
@@ -11,39 +13,53 @@ try:
         print("4. Salir")
 
         opcion = int(input("Escoja la opcion: "))
+
         if opcion == 1:
-                print("\n--- ESTADO DEL PARQUEADERO ---")
-                for i in range(TOTAl_ESPACIOS):
-                    print("Espacio", i + 1, ":", parqueadero[i])
+            print("\n--- ESTADO DEL PARQUEADERO ---")
+            
+            with open("parqueadero.txt", "r") as park:
+                
+                for i, matricula in enumerate(park): 
+                    print("Espacio", i + 1, ":", matricula)
 
         elif opcion == 2:
-                placa = input("Ingrese la placa: ")
-                
-                if "Libre" in parqueadero:
-                    posicion = parqueadero.index("Libre")
-                    parqueadero[posicion] = placa
-                    print("Vehículo ingresado en el espacio", posicion + 1)
-                else:
-                    print("Parqueadero lleno")
+            placa = input("Ingrese la placa: ")
+
+            if "Libre" in parqueadero:
+                posicion = parqueadero.index("Libre")
+                parqueadero[posicion] = placa
+                print("Vehículo ingresado en el espacio", posicion + 1)
+
+                archivo = open("parqueadero.txt", "w")
+                for i in range(TOTAL_ESPACIOS):
+                    archivo.write(parqueadero[i] + "\n")
+                archivo.close()
+
+            else:
+                print("Parqueadero lleno")
 
         elif opcion == 3:
-                placa = input("Ingrese la placa a retirar: ")
-                
-                if placa in parqueadero:
-                    posicion = parqueadero.index(placa)
-                    parqueadero[posicion] = "Libre"
-                    print("Vehículo retirado del espacio", posicion + 1)
-                else:
-                    print("Placa no encontrada")
-        elif opcion == "4":
-                    print("Sistema finalizado")
+            placa = input("Ingrese la placa a retirar: ")
 
-                    with open("Registro_parqueadero.txt", "w") as archivo:
-                        for placa in parqueadero:
-                                archivo.write(placa + "\n")
-                                print("Datos guardados en 'Registro_parqueadero.txt'.programa terminado ")
-                    break
+            if placa in parqueadero:
+                posicion = parqueadero.index(placa)
+                parqueadero[posicion] = "Libre"
+                print("Vehículo retirado del espacio", posicion + 1)
+
+                archivo = open("parqueadero.txt", "w")
+                for i in range(TOTAL_ESPACIOS):
+                    archivo.write(parqueadero[i] + "\n")
+                archivo.close()
+
+            else:
+                print("Placa no encontrada")
+
+        elif opcion == 4:
+            print("Sistema finalizado")
+            break
+
         else:
-                    print("Opción inválida")
+            print("Opción inválida")
+
 except ValueError:
     print("ERROR")
